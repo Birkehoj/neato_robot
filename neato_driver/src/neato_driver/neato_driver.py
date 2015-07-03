eb6444c14d2b41fef1fdae60c175d600a1dd8424
wphan <williamphan8@hotmail.com>
2015-07-02 17:43:40 -0700

    Update neato_driver.py
    Fixed indexing of 'vals', if condition was defaulting to false because vals[0].alpha would fail always, resulting in a broken odometry message

--- neato_driver/src/neato_driver/neato_driver.py
+++ neato_driver/src/neato_driver/neato_driver.py
@@ -137,7 +137,7 @@ class xv11():
         response = self.readResponseString()
         for line in response.splitlines():
             vals = line.split(",")
-            if len(vals) >= 2 and vals[0].isalpha() and vals[1].isdigit():
+            if len(vals) >= 2 and vals[0][0].isalpha() and vals[1].isdigit():
                 self.state[vals[0]] = int(vals[1])
 
     def getMotors(self):
